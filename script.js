9e943f41ec8805f2e5061c54aa32bddf4acdf181
Tabithawa <tabitha.wainaina@student.moringaschool.com>
2021-08-29 01:29:57 +0300

    create functions and connect js file to html file

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 let countEl = document.getElementById("count");
 let count = 0;
+let saveCountEl = document.getElementById("saveCount");
 
 function increment() {
     count += 1;
     countEl.textContent = count;
+}
+
+function decrement() {
+    count -= 1;
+    countEl.textContent = count;
+}
+
+function saveCount() {
+    saveCountEl.textContent += count + " - ";
+    count = 0;
+    countEl.textContent = count;
+
 }
